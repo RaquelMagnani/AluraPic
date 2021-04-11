bb8e2cf84940cc7f55d53d4d1e364ddb6700d365
Raquel Magnani <quel.magnani88@gmail.com>
2021-04-11 19:30:01 -0300

    add photo.component and images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,5 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-
-  title = "AluraPic";
-  description="Floresta";
-  url="https://images.unsplash.com/photo-1618035881605-dfe8d7eb387b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" ;
+  
 }
